e4c1c4ce56a425b1912b530d4ff7ebe660940578
cys <gsp.cys@gmail.com>
2020-03-31 17:44:36 +0800

    SendToServer

---
+++ app/src/main/java/com/grusoft/hitrack/SendToServer.java
@@ -0,0 +1,55 @@
+package com.grusoft.hitrack;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/***
+ * https://stackoverflow.com/questions/35390928/how-to-send-json-object-to-the-server-from-my-android-app
+ */
+public class SendToServer extends AsyncTask<String, Void, String> {
+    @Override
+    protected String doInBackground(String... params) {
+        String data = "";
+        HttpURLConnection httpURLConnection = null;
+        try {
+            httpURLConnection = (HttpURLConnection) new URL(params[0]).openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoOutput(true);
+
+            DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
+            wr.writeBytes("PostData=" + params[1]);
+            wr.flush();
+            wr.close();
+
+            InputStream in = httpURLConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(in);
+
+            int inputStreamData = inputStreamReader.read();
+            while (inputStreamData != -1) {
+                char current = (char) inputStreamData;
+                inputStreamData = inputStreamReader.read();
+                data += current;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (httpURLConnection != null) {
+                httpURLConnection.disconnect();
+        }
+        }
+
+        return data;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        Log.e("TAG", result); // this is expecting a response code to be sent from your server upon receiving the POST data
+    }
+}
