72f906756c844f570a59c5a9cb9c2a2ddc4b1519
cys <gsp.cys@gmail.com>
2020-03-31 11:13:37 +0800

    first commit

---
+++ app/src/main/java/com/grusoft/hitrack/MainActivity.java
@@ -0,0 +1,14 @@
+package com.grusoft.hitrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
